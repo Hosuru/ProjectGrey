85f3c6479753d1e0c8e5ef32c4ae28f7b78d2c41
Hosuru <matiasddivano@gmail.com>
2024-12-11 15:57:44 -0300

    added the helpers

---
+++ ProjectGreyHelper/Test.java
@@ -0,0 +1,21 @@
+package ProjectGreyHelper;
+
+public class Test{
+    public static void main(String[] args){
+
+        String[][] matrix = MyResources.scanNow("ProjectGreyHelper\\Test.csv", ",", 3, 4);
+
+        for (String[] x : matrix) {
+            for (String y : x) {
+                System.out.print(y + " ");
+            }
+            System.out.print("\n");
+        }
+
+        String[] line = MyResources.scanLine("ProjectGreyHelper\\Test.csv", ",", 1);
+        
+        for (String x : line) {
+            System.out.print(x + " ");
+        }
+    }
+}
